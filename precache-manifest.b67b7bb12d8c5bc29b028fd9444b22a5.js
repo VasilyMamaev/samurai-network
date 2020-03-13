6855e9cfee6ee1870dbf859e155ba02a1d22421a
VasilyMamaev <mamaev93@mail.ru>
2020-03-13 12:13:34 +0500

    Updates

---
+++ precache-manifest.b67b7bb12d8c5bc29b028fd9444b22a5.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88ed9ee1c604960689cfbceb3bbe7b62",
+    "url": "/samurai-network/index.html"
+  },
+  {
+    "revision": "5042d5c8e4165cd66427",
+    "url": "/samurai-network/static/css/main.a5a0c3fe.chunk.css"
+  },
+  {
+    "revision": "d742f286a681d579cf46",
+    "url": "/samurai-network/static/js/2.efbafce4.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/samurai-network/static/js/2.efbafce4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5042d5c8e4165cd66427",
+    "url": "/samurai-network/static/js/main.1a221d97.chunk.js"
+  },
+  {
+    "revision": "c37d3543d3c76059f603",
+    "url": "/samurai-network/static/js/runtime-main.280a9cb8.js"
+  },
+  {
+    "revision": "7ec022a249aa029ecfafdcd6172ae22f",
+    "url": "/samurai-network/static/media/facebook-disabled.7ec022a2.svg"
+  },
+  {
+    "revision": "54ec0b8242aebcef28efd193d1fad87f",
+    "url": "/samurai-network/static/media/facebook.54ec0b82.svg"
+  },
+  {
+    "revision": "1601870a5faf59056550564d8a16beed",
+    "url": "/samurai-network/static/media/github-disabled.1601870a.svg"
+  },
+  {
+    "revision": "26fa076cd5ea979893c3842a97085aff",
+    "url": "/samurai-network/static/media/github.26fa076c.svg"
+  },
+  {
+    "revision": "be5be7c0540ef2bde9eff32468c93fb6",
+    "url": "/samurai-network/static/media/instagram-disabled.be5be7c0.svg"
+  },
+  {
+    "revision": "c733f4a0d9a955af8209a301b898c630",
+    "url": "/samurai-network/static/media/instagram.c733f4a0.svg"
+  },
+  {
+    "revision": "b51934039a582d68229ccd4791f33107",
+    "url": "/samurai-network/static/media/loading-cube.b5193403.svg"
+  },
+  {
+    "revision": "42f0c337248b672d342f24b5ef29c2b3",
+    "url": "/samurai-network/static/media/login-background.42f0c337.jpg"
+  },
+  {
+    "revision": "93b997c66312ce17ea7af2a56f41eb70",
+    "url": "/samurai-network/static/media/mainLink-disabled.93b997c6.svg"
+  },
+  {
+    "revision": "b06bcb5759bd96811d63e5c82851829b",
+    "url": "/samurai-network/static/media/mainLink.b06bcb57.svg"
+  },
+  {
+    "revision": "ae70a063e9770e4c3085bcd05bbb67af",
+    "url": "/samurai-network/static/media/no-avatar-img.ae70a063.jpg"
+  },
+  {
+    "revision": "92c49125d211f769cf6b42ac1a405e0f",
+    "url": "/samurai-network/static/media/post-like.92c49125.svg"
+  },
+  {
+    "revision": "2ce51e1ee95af6cdb85d3d7a17736c09",
+    "url": "/samurai-network/static/media/twitter-disabled.2ce51e1e.svg"
+  },
+  {
+    "revision": "732c25043593be65d1eb1adfffb9e0f6",
+    "url": "/samurai-network/static/media/twitter.732c2504.svg"
+  },
+  {
+    "revision": "db0605ccb4634871a6f98f2ab557ef52",
+    "url": "/samurai-network/static/media/vk-disabled.db0605cc.svg"
+  },
+  {
+    "revision": "c0768a948980ebe7654064712a3718e0",
+    "url": "/samurai-network/static/media/vk.c0768a94.svg"
+  },
+  {
+    "revision": "7056f384941f72d886a2b40c32b3acf2",
+    "url": "/samurai-network/static/media/website-disabled.7056f384.svg"
+  },
+  {
+    "revision": "85988b1beab951ca1288a3b9555f69e2",
+    "url": "/samurai-network/static/media/website.85988b1b.svg"
+  },
+  {
+    "revision": "4386719934509dcb34d0fde448a6014b",
+    "url": "/samurai-network/static/media/youtube-disabled.43867199.svg"
+  },
+  {
+    "revision": "4f7e55d609b72fb803748667a51bd665",
+    "url": "/samurai-network/static/media/youtube.4f7e55d6.svg"
+  }
+]);
